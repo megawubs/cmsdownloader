fe3483f5e7b95ecc52b4697504e397e92053dd1d
Bram Wubs <bwubs@me.com>
2012-11-22 14:29:03 +0100

    Gets the newest version of Magento and Joomla, downloads them.

--- getmagandjoo.py
+++ getmagandjoo.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python
 
-import urllib
-import urllib2
+import urllib, urllib2, os, sys
 from pprint import pprint
 from HTMLParser import HTMLParser
-import requests, xml.etree.cElementTree as et
-from requests.auth import HTTPBasicAuth
 
 
 class HttpBot:
     """an HttpBot represents one browser session, with cookies."""
     def __init__(self):
-        cookie_handler= urllib2.HTTPCookieProcessor()
-        redirect_handler= urllib2.HTTPRedirectHandler()
-        self._opener = urllib2.build_opener(redirect_handler, cookie_handler)
+        cookie_handler   = urllib2.HTTPCookieProcessor()
+        redirect_handler = urllib2.HTTPRedirectHandler()
+        self._opener     = urllib2.build_opener(redirect_handler, cookie_handler)
 
     def GET(self, url):
         return self._opener.open(url).read()
@@ -23,57 +20,82 @@ class HttpBot:
 
 
 class downloader:
-    def __init__(self):
-        self.bot = HttpBot()
+    def __init__(self, basePath):
+        self.bot                = HttpBot()
         self.magentoDownloadUrl = 'https://www.magentocommerce.com/download'
-
-        self.downloadedFiles = []
+        self.versions           = {'Magento':[], 'Joomla':[]}
+        self.downloadedFiles    = []
+        self.basePath           = basePath
+        self.downloadFiles      = False
+        if not os.path.isdir(self.basePath):
+            print 'Path to download to does not exist'
+            exit()
 
     def getMage(self):
         version = self.getMageLatestVersion()
-        theUrl = 'http://www.magentocommerce.com/downloads/assets/'+version+'/magento-'+version+'.tar.gz'
-        self.download(theUrl)
+        if not self.checkVersion("Magento", version):
+            self.versions['Magento'].append(version)
+            theUrl  = 'http://www.magentocommerce.com/downloads/assets/'+version+'/magento-'+version+'.tar.gz'
+            self.download(theUrl)
 
     def getMageLatestVersion(self):
-        downloadPage = self.bot.GET(self.magentoDownloadUrl)
-        parser = MagentoParser()
+        downloadPage    = self.bot.GET(self.magentoDownloadUrl)
+        parser          = MagentoParser()
         parser.feed(downloadPage)
         allMageVersions = parser.versions
-        newest = allMageVersions[0]
+        newest          = allMageVersions[0]
         return newest
 
 
     def getJom(self):
         jomDownloadPage = 'http://www.joomla.org/download.html';
-        html = self.bot.GET(jomDownloadPage)
-        parser = JoomlaParser()
+        html            = self.bot.GET(jomDownloadPage)
+        parser          = JoomlaParser()
         parser.feed(html)
         for link in parser.downloadLinks:
-            self.download(link)
+            filename = link.split('/')[-1]
+            version  = filename.split('_')[1]
+            self.versions['Joomla'].append(version)
+            if not self.checkVersion('Joomla', version):
+                self.download(link)
 
     def download(self, url):
         file_name = url.split('/')[-1]
-        u = urllib2.urlopen(url)
-        f = open(file_name, 'wb')
-        meta = u.info()
-        file_size = int(meta.getheaders("Content-Length")[0])
-        print "Downloading: %s Bytes: %s" % (file_name, file_size)
+        print "downloading "+file_name
+        if self.downloadFiles:
+            u         = urllib2.urlopen(url)
+            f         = open(file_name, 'wb')
+            meta      = u.info()
+            file_size = int(meta.getheaders("Content-Length")[0])
+            print "Downloading: %s Bytes: %s" % (file_name, file_size)
+            file_size_dl = 0
+            block_sz     = 8192
+            while True:
+                buffer = u.read(block_sz)
+                if not buffer:
+                    break
 
-        file_size_dl = 0
-        block_sz = 8192
-        while True:
-            buffer = u.read(block_sz)
-            if not buffer:
-                break
+                file_size_dl += len(buffer)
+                f.write(buffer)
+                status       = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+                status       = status + chr(8)*(len(status)+1)
+                print status,
 
-            file_size_dl += len(buffer)
-            f.write(buffer)
-            status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-            status = status + chr(8)*(len(status)+1)
-            print status,
-
-        f.close()
+            f.close()
         self.downloadedFiles.append(file_name)
+
+    def checkVersion(self, package, version):
+        path = os.path.join(self.basePath, os.path.join(package, version))
+        print path
+        return os.path.exists(path)
+
+    def handleFiles(self):
+        if os.path.isdir(self.basePath):
+            # magePath = os.path.join(self.basePath, os.path.join('Magento', self.versions['Magento'][0]))
+            for package in self.versions:
+                for version in self.versions[package]:
+                    path = os.path.join(self.basePath, os.path.join(package, version))
+                    # print path
 
 # create a subclass and override the handler methods
 class JoomlaParser(HTMLParser):
@@ -101,7 +123,7 @@ class MagentoParser(HTMLParser):
                         self.versions.append(version)
 
 if __name__ == "__main__":
-    info = {'mage':{'name':'h.hofman@bsmedia.nl','passwd':'n)O-SV3s'}, 'jom':{'name':'', 'passwd':''}}
-    d = downloader()
+    d = downloader('/Volumes/Extra HDD/BSMedia/')
     d.getMage()
     d.getJom()
+    # d.handleFiles()
