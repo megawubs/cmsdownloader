f056e66399125e76b58cb76b45a33fcd9a6f0cba
Bram Wubs <bwubs@me.com>
2012-12-03 12:39:55 +0100

    Renamed the log file to "CMS Downloader.log"

--- app/MyLogger.py
+++ app/MyLogger.py
@@ -3,8 +3,8 @@ import logging, os
 class MyLogger:
 
     def __init__(self, location):
-        self.logger = logging.getLogger('getmageandjo')
-        handler = logging.FileHandler(os.path.join(location,'Magento Joomla Downloader.log'))
+        self.logger = logging.getLogger('cmsdownloader')
+        handler = logging.FileHandler(os.path.join(location,'CMS Downloader.log'))
         formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
         handler.setFormatter(formatter)
         self.logger.addHandler(handler)
